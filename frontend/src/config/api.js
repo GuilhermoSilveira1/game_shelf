55f05e47ee252d3baf467fcb931cfb0e71a43869
Guilhermo Silveira <guilhermosilveira@gmail.com>
2026-08-07 11:20:06 -0300

    Ajuste na chamada da API

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,8 +1,8 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = process.env.NEXT_API_URL;
 
 if (!API_URL) {
   throw new Error(
-    "A variável NEXT_PUBLIC_API_URL não está configurada."
+    "A variável NEXT_API_URL não está configurada."
   );
 }
 
